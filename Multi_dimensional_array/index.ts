348fd64d6bb84edeecd5c3ddd86cd530a3ff464b
SucaadSalaadcis <sucaadsalaad520@gmail.com>
2025-01-26 16:30:54 +0300

    added multidimensional_array

---
+++ Multi_dimensional_array/index.ts
@@ -0,0 +1,7 @@
+const singleDi: number[] = [1, 2, 3];
+const multiDi: number[][] = [[1, 3, 5]];
+const triple: number[][][] = [[[3, 4, 6]]];
+
+console.log(singleDi);
+console.log(multiDi);
+console.log(triple);
