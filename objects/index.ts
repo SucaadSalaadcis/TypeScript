f28a6fb236fe832eb0739269ce5c8dd2913b4199
SucaadSalaadcis <sucaadsalaad520@gmail.com>
2025-01-26 16:46:23 +0300

    objects

---
+++ objects/index.ts
@@ -0,0 +1,8 @@
+const person: { firstName: string; lastName: string; age: number } = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 30
+}
+
+console.log(`Name: ${person.firstName} ${person.lastName}, Age: ${person.age}`);
+
