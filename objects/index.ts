dc0c179ce85649e9c66e8ec7d625b6f14ee0ef33
SucaadSalaadcis <sucaadsalaad520@gmail.com>
2025-01-27 09:38:08 +0300

    objects as funcion

--- objects/index.ts
+++ objects/index.ts
@@ -5,4 +5,20 @@ const person: { firstName: string; lastName: string; age: number } = {
 }
 
 console.log(`Name: ${person.firstName} ${person.lastName}, Age: ${person.age}`);
+// console.log(person);
 
+// objects as funcion 
+function printUser(): { name: string; age: number; location: string } {
+    return {
+        name: 'Xuxn',
+        age: 20,
+        location: 'China'
+    }
+}
+
+// const res2 = printUser().name;
+const res2 = printUser();
+console.log(res2);
+
+// in typeScript code :  make sure to complete your code or you get an error so don't focus it fisrt
+// unless you complete your code and there is error
