273358b34d6a92bf5a774deb918e10bf02fbed80
SucaadSalaadcis <sucaadsalaad520@gmail.com>
2025-01-27 12:19:26 +0300

    added destructuring tubles

--- Tuples/index.ts
+++ Tuples/index.ts
@@ -8,9 +8,11 @@ user5 = ["Alice", 25]; // ✅ Correct
 // user5 = ['num', 4, 'test'] // ❌ Error:  target allows only 2
 
 
+
 // Tuple with Multiple Types
 let product: [number, string, boolean];
 product = [101, "Laptop", true]; // ✅ Correct
+
 
 
 // Tuple with Optional Elements
@@ -19,4 +21,11 @@ user6 = ["Alice"];         // ✅ Second element is optional
 user6 = ["Alice", 30];     // ✅ Both elements are provided
 // user = [30, "Alice"];  // ❌ Error: Order and types must match
 
-let myTupl
+
+
+// Destructuring Tuples
+let user7: [string, number] = ["Alice", 25];
+const [userName, age] = user7;
+
+console.log(userName); // "Alice"
+console.log(age);  // 25
